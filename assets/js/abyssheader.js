625b3efd8e623ef811a3f726cf444c0ccb4b5b84
NSDawn <tnahsin.airus@gmail.com>
2023-05-27 08:28:07 -0700

    some finalizations

--- assets/js/abyssheader.js
+++ assets/js/abyssheader.js
@@ -1,1 +1,17 @@
-return;
+let abyssHeaderIconList = (localStorage.getItem("abyssHeaderIconList") ?? "_home_lang").split("_")
+const ABYSSHEADER_ICONBOX = document.getElementsByClassName("icon_box")[0]
+
+
+for (icon of abyssHeaderIconList) {
+
+}
+
+
+
+const ICONS_REF = {
+    "back": {
+        'src': "_assets_file_path/img/icon/ABYSS/back.png",
+        'alt': "back icon",
+        'href': "../",
+    }
+}
