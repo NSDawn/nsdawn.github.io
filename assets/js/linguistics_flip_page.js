2dfa50298b87445a10d44117213dda80a83b9f67
NSDawn <tnahsin.airus@gmail.com>
2023-06-03 13:55:59 -0700

    Tamil liquid project updates

--- assets/js/linguistics_flip_page.js
+++ assets/js/linguistics_flip_page.js
@@ -2,17 +2,26 @@ const LING_TITLE = document.getElementsByClassName("ling_mainbox_title")[0];
 const LING_DESC = document.getElementsByClassName("ling_mainbox_text")[0];
 const LING_ICON = document.getElementsByClassName("ling_mainbox_icon icon")[0];
 const LING_LINK = document.getElementsByClassName("ling_mainbox_go_link")[0];
+const LING_MAINBOX = document.getElementsByClassName("ling_mainbox")[0]
+console.log(LING_MAINBOX.classList.add('quickspin'))
 
 const LING_LINKS_REF = {
     "LINGUISTICS/PROJECTS_1": "tamil_liquids",
     "LINGUISTICS/PROJECTS_2": "japanese_vowels",
     "LINGUISTICS/PROJECTS_3": "feature_lookup",
 }
+
+setTimeout(function() {LING_MAINBOX.classList.remove('quickspin');}, 505)
+let curr_ling_page = 'LINGUISTICS/PROJECTS_1'
 function lingFlipPage(idname) {
+    if (idname == curr_ling_page) return;
+    curr_ling_page = idname;
     LING_TITLE.id = idname + "_TITLE";
     LING_DESC.id = idname + "_DESC";
     LING_ICON.src = "../assets/img/icon/" + idname + ".png";
     LING_LINK.href = LING_LINKS_REF[idname];
+    LING_MAINBOX.classList.add('quickspin');
+    setTimeout(function() {LING_MAINBOX.classList.remove('quickspin');}, 505)
     updateData(LANG_DATA);
 }
 
