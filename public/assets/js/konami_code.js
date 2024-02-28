a9de470c9607ba47b410af39ecfe0ac5c1848102
NSDawn <tnahsin.airus@gmail.com>
2024-02-27 19:19:54 -0800

    adding konami code easter egg

--- public/assets/js/konami_code.js
+++ public/assets/js/konami_code.js
@@ -1,5 +1,36 @@
-// // // // // // // // // // // // // // // // // // // //
-// konami_code.js                                        //
-// by N.S. Dawn                                          //
-// A translation of previous python FeatureLookup.py     //
-// // // // // // // // // // // // // // // // // // // //
+let codes = [
+    {
+        sequence: ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"],
+        href: "https://www.youtube.com/watch?v=U1dirHGODpM",
+        index: 0,
+    },
+    {
+        sequence: ["c", "h","r","i","s","t","i","a","n"],
+        href: "https://www.cglauch.dev",
+        index: 0,
+    },
+    {
+        sequence: ["s", "u","s","s","y","b","a","k","a"],
+        href: "https://www.youtube.com/watch?v=Ru_j8oRi-rY",
+        index: 0,
+    }
+]
+
+let sequenceIndex = 0;
+
+document.addEventListener("keydown", (e) => {
+    
+    const ignoredChars = [" "];
+    if (ignoredChars.includes(e.key)) return;
+
+    codes.forEach((code) => {
+        if (code.sequence[code.index] === e.key) {
+            code.index += 1;
+            if (code.index === code.sequence.length) {
+                location.href = code.href;
+            }
+        } else {
+            code.index = 0;
+        }
+    })
+});
